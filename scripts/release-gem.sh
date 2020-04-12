1aaed5be02ddfa96a2f4c95ded451065f0df48b1
Aori Nevo <aorinevo@gmail.com>
2020-04-12 16:58:35 -0400

    fix: add support for non-darwin OS types.

--- scripts/release-gem.sh
+++ scripts/release-gem.sh
@@ -4,6 +4,11 @@ mkdir -p ~/.gem
 touch ~/.gem/credentials
 chmod 600 ~/.gem/credentials
 echo ":rubygems_api_key: Bearer ${RUBYGEMS_API_KEY}" >> ~/.gem/credentials
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i "" -e "s/.*s.version.*/    s.version     = $(cat package.json | jq .version)/" semantic_release_gem_demo.gemspec
+else
+  sed -i -e "s/.*s.version.*/    s.version     = $(cat package.json | jq .version)/" semantic_release_gem_demo.gemspec
+fi
 sed -i "" "s/.*s.version.*/    s.version     = $(cat package.json | jq .version)/" semantic_release_gem_demo.gemspec
 gem build *.gemspec
 gem push --key rubygems_api_key *.gem
