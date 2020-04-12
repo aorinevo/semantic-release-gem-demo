3b5506fef99bf1c075b7e0f750ef143c923eb80c
Aori Nevo <aorinevo@gmail.com>
2020-04-12 13:35:51 -0400

    feat: add npm publishing.

--- .releaserc.js
+++ .releaserc.js
@@ -46,11 +46,14 @@ const releaseConfig = {
         ],
         '@semantic-release/changelog',
         [
+            '@semantic-release/npm',
+            {
+                npmPublish: false
+            }
+        ],
+        [
             '@semantic-release/git',
             {
-                assets: [
-                    'CHANGELOG.md'
-                ],
                 message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}'
             }
         ],
