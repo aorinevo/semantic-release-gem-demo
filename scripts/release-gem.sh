3d9f6e4ee31d1d6a356419837f83ca2ee23f0f7e
Aori Nevo <aorinevo@gmail.com>
2020-04-12 17:15:50 -0400

    fix: order of arguments in sed command

--- scripts/release-gem.sh
+++ scripts/release-gem.sh
@@ -7,7 +7,7 @@ echo ":rubygems_api_key: Bearer ${RUBYGEMS_API_KEY}" >> ~/.gem/credentials
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i "" -e "s/.*s.version.*/    s.version     = $(cat package.json | jq .version)/" semantic_release_gem_demo.gemspec
 else
-  sed -i -e "s/.*s.version.*/    s.version     = $(cat package.json | jq .version)/" semantic_release_gem_demo.gemspec
+  sed -i semantic_release_gem_demo.gemspec -e "s/.*s.version.*/    s.version     = $(cat package.json | jq .version)/"
 fi
 sed -i "" "s/.*s.version.*/    s.version     = $(cat package.json | jq .version)/" semantic_release_gem_demo.gemspec
 gem build *.gemspec
